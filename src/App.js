0c0451ecc50a7fa9bfbf588df89dcdaca6e51c77
Aaron Kaplowitz <akaplowitz@alum.cs.umass.edu>
2021-01-24 19:53:41 -0500

    Add action buttons and resume

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import './App.css';
+import Button from "@material-ui/core/Button";
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import resume from './aaron_kaplowitz_resume_02_2021.pdf';
 
-function App() {
+const theme = createMuiTheme({
+  palette: {
+    type: "dark"
+  }
+});
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p className="Aaron-title">
-          Aaron
-        </p>
-      </header>
-    </div>
+    <ThemeProvider theme={ theme }>
+      <div className="App">
+        <header className="App-header">
+          <p className="Aaron-title">
+            Aaron Kaplowitz
+          </p>
+        </header>
+        <div className={ 'Prompt-and-Buttons' }>
+          <h3>What brings you by?</h3>
+          <Button variant={ 'outlined' }>Learn about Aaron</Button>
+          <Button href={ resume } target={ '_blank' } variant={ 'outlined' }>Just a résumé</Button>
+        </div>
+      </div>
+    </ThemeProvider>
   );
 }
 
